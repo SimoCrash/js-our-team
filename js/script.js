95371b2377b9c1f1315f38ab3e8a53a8e8cea246
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-07 15:45:53 +0100

    MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -2,40 +2,42 @@ const arrWorkers = [
     {
         name:"Wayne Barnett",
         task:"Founder & CEO",	
-        photo: `<img src="img/wayne-barnett-founder-ceo.jpg">`,
+        photo:`<img src="img/wayne-barnett-founder-ceo.jpg">`,
 
     },
 
     {
         name:"Angela Caroll",
         task:"Chief Editor",	
-        photo: `<img src="img/angela-caroll-chief-editor.jpg">`, 
+        photo:`<img src="img/angela-caroll-chief-editor.jpg">`, 
     },
 
     {
         name:"Walter Gordon",
         task:"Office Manager",	
-        photo: `<img src="img/walter-gordon-office-manager.jpg">`,
+        photo:`<img src="img/walter-gordon-office-manager.jpg">`,
     },
 
     {
         name:"Angela Lopez",
         task:"Social Media Manager",	
-        photo: `<img src="img/angela-lopez-social-media-manager.jpg">`,
+        photo:`<img src="img/angela-lopez-social-media-manager.jpg">`,
     },
 
     {
         name:"Scott Estrada",
         task:"Developer",	
-        photo: `<img src="img/scott-estrada-developer.jpg">`,
+        photo:`<img src="img/scott-estrada-developer.jpg">`,
     },
 
     {
         name:"Barbara Ramos",
         task:"Graphic Designer",	
-        photo: `<img src="img/barbara-ramos-graphic-designer.jpg">`,
+        photo:`<img src="img/barbara-ramos-graphic-designer.jpg">`,
     },
 ]
+
+console.log(arrWorkers);
 
 		
  		
